62a1f68e7d18a5886ed27f14be615774c270fac6
yeonsuBaek <soothbugger@gmail.com>
2024-05-05 08:03:56 +0900

    📦 Install tanstack query

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,13 +1,20 @@
 'use client'
 import { I18nextProvider } from 'react-i18next'
 import i18n from '@/locales/i18n'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 interface ProviderProps {
   children: React.ReactNode | React.ReactNode[]
 }
 
 function Providers({ children }: ProviderProps) {
-  return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
+  const queryClient = new QueryClient()
+
+  return (
+    <I18nextProvider i18n={i18n}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </I18nextProvider>
+  )
 }
 
 export default Providers
