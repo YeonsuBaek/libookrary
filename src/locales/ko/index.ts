039129134a3883ebde9b58c71deace38159c83a9
yeonsuBaek <soothbugger@gmail.com>
2024-05-04 13:52:01 +0900

    ✨ Implement Book page

--- src/locales/ko/index.ts
+++ src/locales/ko/index.ts
@@ -1,9 +1,11 @@
 import home from './home.json'
 import header from './header.json'
 import user from './user.json'
+import book from './book.json'
 
 export default {
   home,
   header,
   user,
+  book,
 }
