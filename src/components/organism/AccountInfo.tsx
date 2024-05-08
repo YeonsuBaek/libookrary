36ef64ab86a70f5e8d101a6b8d263173afbd6151
yeonsuBaek <soothbugger@gmail.com>
2024-05-08 09:54:02 +0900

    🚧 Fetch userInfo and apply i18n

--- src/components/organism/AccountInfo.tsx
+++ src/components/organism/AccountInfo.tsx
@@ -1,20 +1,32 @@
 'use client'
+import { getUserInfo } from '@/apis/user'
 import { Button } from '@yeonsubaek/yeonsui'
+import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 function AccountInfo() {
   const { t } = useTranslation()
+  const [email, setEmail] = useState('')
+  const [nickname, setNickname] = useState('')
+
+  useEffect(function fetchUserInfo() {
+    ;(async () => {
+      const info = await getUserInfo()
+      setEmail(info?.email || '')
+      setNickname(info?.nickname || '')
+    })()
+  }, [])
 
   return (
     <>
       <dl className="account-list">
         <div className="account-item">
-          <dt className="account-title">이메일</dt>
-          <dd className="account-detail">1234@gmail.com</dd>
+          <dt className="account-title">{t('user.form.email')}</dt>
+          <dd className="account-detail">{email}</dd>
         </div>
         <div className="account-item">
-          <dt className="account-title">닉네임</dt>
-          <dd className="account-detail">이름</dd>
+          <dt className="account-title">{t('user.form.nickname')}</dt>
+          <dd className="account-detail">{nickname}</dd>
         </div>
       </dl>
       <div className="account-button">
