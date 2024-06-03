677c417d948dc105c9539564c605ce9a4daa72f9
yeonsuBaek <soothbugger@gmail.com>
2024-06-03 13:48:44 +0900

    💄 Hide a minus button when have no onRemove

--- src/components/book/Bookmark/BookmarkList.tsx
+++ src/components/book/Bookmark/BookmarkList.tsx
@@ -6,12 +6,12 @@ interface BookmarkListProps {
   onRemove?: (id: number) => void
 }
 
-function BookmarkList({ bookmarks, onRemove = () => {} }: BookmarkListProps) {
+function BookmarkList({ bookmarks, onRemove }: BookmarkListProps) {
   return (
     <ul className="bookmark-list">
       {bookmarks &&
         bookmarks.map(({ id, page, content }: BookmarkType) => {
-          return <Bookmark key={id} page={page} text={content} onRemove={() => onRemove(id)} />
+          return <Bookmark key={id} page={page} text={content} onRemove={onRemove ? () => onRemove(id) : undefined} />
         })}
     </ul>
   )
