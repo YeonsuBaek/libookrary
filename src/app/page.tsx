9fe3fb0c4eb80307285302ba72c2475fd9c6c922
yeonsuBaek <soothbugger@gmail.com>
2024-06-03 13:38:20 +0900

    🐛 Move Search from Home to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Library from '@/components/book/Library'
-import Search from '@/components/book/Search'
 
 export default function Home() {
   return (
     <div className="home">
       <Library />
-      <Search />
     </div>
   )
 }
