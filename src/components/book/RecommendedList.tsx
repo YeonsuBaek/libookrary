c2cc801666fc71c8f4329b5a681b1366e9cc3d01
yeonsuBaek <soothbugger@gmail.com>
2024-06-19 15:34:59 +0900

    ♻️ Refactor BookCardListSkeleton

--- src/components/book/RecommendedList.tsx
+++ src/components/book/RecommendedList.tsx
@@ -1,6 +1,5 @@
-import { useTranslation } from 'react-i18next'
 import BookCardList from './BookCard/BookCardList'
-import BookCardSkeleton from './BookCard/BookCardSkeleton'
+import BookCardSkeletonList from './BookCard/BookCardSkeletonList'
 
 interface RecommendedListProps {
   title: string
@@ -8,13 +7,6 @@ interface RecommendedListProps {
 }
 
 function RecommendedList({ title, books }: RecommendedListProps) {
-  const { t } = useTranslation('')
-  const skeletonList = [
-    { id: 'book-card-skeleton-1', title: t('book.skeleton.title'), info: t('book.skeleton.author') },
-    { id: 'book-card-skeleton-2', title: t('book.skeleton.title'), info: t('book.skeleton.author') },
-    { id: 'book-card-skeleton-3', title: t('book.skeleton.title'), info: t('book.skeleton.author') },
-    { id: 'book-card-skeleton-4', title: t('book.skeleton.title'), info: t('book.skeleton.author') },
-  ]
   return (
     <div className="recommended-list">
       <h2 className="recommended-list-title">{title}</h2>
@@ -22,7 +14,7 @@ function RecommendedList({ title, books }: RecommendedListProps) {
         {books.length > 0 ? (
           <BookCardList sort="nowrap" books={books} isAddRoute />
         ) : (
-          skeletonList.map((skeleton) => <BookCardSkeleton {...skeleton} />)
+          <BookCardSkeletonList sort="nowrap" />
         )}
       </div>
     </div>
