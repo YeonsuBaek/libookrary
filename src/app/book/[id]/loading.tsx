006d83eed75e02e44a2c51348b164ba6a199b5ea
YeonsuBaek <soothbugger@gmail.com>
2024-06-28 14:36:29 +0900

    ✨ Implement Skeleton component

---
+++ src/app/book/[id]/loading.tsx
@@ -0,0 +1,7 @@
+import Skeleton from '@/components/common/Skeleton'
+
+function Loading() {
+  return <Skeleton />
+}
+
+export default Loading
---
+++ src/components/common/Skeleton.tsx
@@ -0,0 +1,12 @@
+function Skeleton() {
+  return (
+    <div className="skeleton">
+      <span className="skeleton-line"></span>
+      <span className="skeleton-line"></span>
+      <span className="skeleton-line"></span>
+      <span className="skeleton-line"></span>
+    </div>
+  )
+}
+
+export default Skeleton
