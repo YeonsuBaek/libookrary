bd4233deab92a594030b98a6574200417573c859
YeonsuBaek <soothbugger@gmail.com>
2024-06-28 14:37:58 +0900

    🚸 Improve BookPage to server component

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,7 +1,31 @@
-import BookPage from '@/components/book/BookPage'
+import { getBookInfo } from '@/apis/book'
+import BookInfo from '@/components/book/BookInfo'
+import BookPageButtons from '@/components/book/BookPageButtons'
+import ReadingInfo from '@/components/book/ReadingInfo'
+import PageTitle from '@/components/common/PageTitle'
+import { Suspense } from 'react'
+import Loading from './Loading'
 
-function page({ params }: { params: { id: string } }) {
-  return <BookPage id={params.id} />
+async function page({ params }: { params: { id: string } }) {
+  const { id } = params
+  const info = await getBookInfo({ isbn: id })
+
+  return (
+    <Suspense fallback={<Loading />}>
+      <PageTitle route={info.title} />
+      <ReadingInfo id={id} title={info.title} cover={info.cover} />
+      <BookInfo
+        author={info.author}
+        publisher={info.publisher}
+        pubdate={info.pubdate}
+        desc={info.desc}
+        category={info.category}
+        isbn={id}
+        price={info.price}
+      />
+      <BookPageButtons id={id} />
+    </Suspense>
+  )
 }
 
 export default page
---
+++ src/components/book/BookPageButtons.tsx
@@ -0,0 +1,53 @@
+'use client'
+import { useRouter } from 'next/navigation'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import onModal from '../common/Modal'
+import { deleteBook } from '@/apis/book'
+import onToast from '../common/Toast'
+import { Button } from '@yeonsubaek/yeonsui'
+
+interface BookPageProps {
+  id: string
+}
+
+function BookPageButtons({ id }: BookPageProps) {
+  const { t } = useTranslation('')
+  const router = useRouter()
+
+  const handleOpenDeleteModal = () => {
+    onModal({
+      message: t('modal.book.delete'),
+      onSave: onDelete,
+    })
+  }
+
+  const onDelete = () => {
+    deleteBook(
+      { isbn: id },
+      {
+        onSuccess: () => {
+          onToast({
+            id: 'delete-success-toast',
+            message: t('toast.book.delete.success'),
+          })
+          router.push('/')
+        },
+        onError: () => onToast({ id: 'delete-error-toast', message: t('toast.book.delete.error'), color: 'error' }),
+      }
+    )
+  }
+
+  return (
+    <div className="book-buttons">
+      <Button variant="link" color="success" onClick={() => router.push(`/book/edit/${id}`)}>
+        {t('book.button.edit')}
+      </Button>
+      <Button variant="link" color="error" onClick={handleOpenDeleteModal}>
+        {t('book.button.delete')}
+      </Button>
+    </div>
+  )
+}
+
+export default BookPageButtons
