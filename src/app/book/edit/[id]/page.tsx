7b7778d66e416f0ffe7ca7f6c8af8bab54329bd7
yeonsuBaek <soothbugger@gmail.com>
2024-07-02 12:37:57 +0900

    🚸 Improve EditBookPage to server component

--- src/app/book/edit/[id]/page.tsx
+++ src/app/book/edit/[id]/page.tsx
@@ -1,7 +1,28 @@
-import EditBook from '@/components/book/EditBook'
+import { getBookInfo } from '@/apis/book'
+import ReadingEdit from '@/components/book/ReadingEdit'
+import PageTitle from '@/components/common/PageTitle'
+import Skeleton from '@/components/common/Skeleton'
+import { Suspense } from 'react'
 
-function page({ params }: { params: { id: string } }) {
-  return <EditBook id={params.id} />
+async function page({ params }: { params: { id: string } }) {
+  const { id } = params
+  const info = await getBookInfo({ isbn: id })
+
+  return (
+    <Suspense fallback={<Skeleton />}>
+      <PageTitle route={info?.title} />
+      <ReadingEdit
+        isbn={id}
+        title={info?.title}
+        cover={info?.cover}
+        defStartDate={info?.startDate}
+        defEndDate={info?.endDate}
+        defBookmarks={info?.bookmarks}
+        defIsRecommended={info?.isRecommended}
+        defWantToReRead={info?.wantToReRead}
+      />
+    </Suspense>
+  )
 }
 
 export default page
