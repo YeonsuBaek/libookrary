f6a87976d6b096bf2c056a74917c4453f9deebb9
yeonsuBaek <soothbugger@gmail.com>
2024-10-06 14:26:13 +0900

    💄 #45 Apply red color to a delete button

--- src/components/book/BookPageButtons.tsx
+++ src/components/book/BookPageButtons.tsx
@@ -43,8 +43,7 @@ function BookPageButtons({ id }: BookPageProps) {
       <Button styleType="ghost" styleVariant="primary" onClick={() => router.push(`/book/edit/${id}`)}>
         {t('book.button.edit')}
       </Button>
-      {/* TODO: RED 색상으로 */}
-      <Button styleType="ghost" styleVariant="secondary" color="error" onClick={handleOpenDeleteModal}>
+      <Button styleType="ghost" color="danger" onClick={handleOpenDeleteModal}>
         {t('book.button.delete')}
       </Button>
     </div>
